7640d40d9b8130c55391bb1168514371322b5d68
bokkle <mitch.r_@hotmail.com>
2023-08-08 02:08:25 -0400

    media queries and finished gallery

--- script.js
+++ script.js
@@ -117,12 +117,114 @@ const toggleModal = () => {
   document.body.classList += " modal--open";
 };
 
-// BURGER MENU 
+// BURGER MENU
 
 const openMenu = () => {
-    document.body.classList += " menu--open"
-}
+  document.body.classList += " menu--open";
+};
 
 const closeMenu = () => {
-    document.body.classList.remove('menu--open')
-}
+  document.body.classList.remove("menu--open");
+};
+
+// GALLERy
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slider");
+  const navLinks = document.querySelectorAll(".slider__nav a");
+  let currentSlide = 0;
+  let manualSlideSelected = false; // Track manual slide selection
+  let autoScrollInterval; // Store the auto-scroll interval
+  let isInViewport = false; // Track if the slider is in the viewport
+
+  // Check if the element is in the viewport
+  function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function scrollToNextSlide() {
+    if (isInViewport && !manualSlideSelected) {
+      currentSlide = (currentSlide + 1) % navLinks.length;
+    }
+    updateSlideOpacity();
+    const slideIndex = currentSlide;
+
+    slider.scrollTo({
+      left: slider.clientWidth * slideIndex,
+      behavior: "smooth",
+    });
+  }
+
+  function startAutoScroll() {
+    autoScrollInterval = setInterval(scrollToNextSlide, 4000);
+  }
+
+  function pauseAutoScroll() {
+    clearInterval(autoScrollInterval);
+  }
+
+  // Use Intersection Observer to detect when the slider enters the viewport
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        isInViewport = true;
+        startAutoScroll();
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+
+  observer.observe(slider);
+
+  function updateSlideOpacity() {
+    navLinks.forEach(function (link, index) {
+      if (index === currentSlide) {
+        link.style.opacity = 1;
+      } else {
+        link.style.opacity = 0.75;
+      }
+    });
+  }
+
+  updateSlideOpacity(); // Set initial opacity
+
+  slider.addEventListener("scroll", function () {
+    if (!manualSlideSelected) {
+      const slideIndex = Math.round(slider.scrollLeft / slider.clientWidth);
+      currentSlide = slideIndex;
+      updateSlideOpacity();
+    }
+  });
+
+  navLinks.forEach(function (link, index) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      if (!manualSlideSelected) {
+        currentSlide = index; // Set the selected slide index
+        manualSlideSelected = true; // Mark manual slide selection
+
+        updateSlideOpacity();
+        pauseAutoScroll(); // Pause the auto-scroll interval
+
+        slider.scrollTo({
+          left: slider.clientWidth * currentSlide,
+          behavior: "smooth",
+        });
+
+        setTimeout(() => {
+          manualSlideSelected = false; // Reset manual slide selection after a delay
+          startAutoScroll(); // Restart the auto-scroll
+        }, 1000); // Delay in milliseconds before re-enabling auto-scroll
+      }
+    });
+  });
+});
+
