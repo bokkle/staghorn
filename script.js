a7ce57e6ea1a847561f5f07eb335c432194f3469
bokkle <mitch.r_@hotmail.com>
2023-08-08 17:18:28 -0400

    donzo?

--- script.js
+++ script.js
@@ -31,54 +31,54 @@ upArrow.onclick = () => {
 
 // MOBILE
 
-let touchStartY = 0;
-let touchEndY = 0;
+// let touchStartY = 0;
+// let touchEndY = 0;
 
-slide.addEventListener("touchstart", (event) => {
-  touchStartY = event.touches[0].clientY;
-});
+// slide.addEventListener("touchstart", (event) => {
+//   touchStartY = event.touches[0].clientY;
+// });
 
-slide.addEventListener("touchend", (event) => {
-  touchEndY = event.changedTouches[0].clientY;
-  handleSwipe();
-});
+// slide.addEventListener("touchend", (event) => {
+//   touchEndY = event.changedTouches[0].clientY;
+//   handleSwipe();
+// });
 
-const handleSwipe = () => {
-  const swipeDistance = touchEndY - touchStartY;
-  if (swipeDistance > 50) {
-    swipeUp();
-  } else if (swipeDistance < -50) {
-    swipeDown();
-  }
-};
+// const handleSwipe = () => {
+//   const swipeDistance = touchEndY - touchStartY;
+//   if (swipeDistance > 50) {
+//     swipeUp();
+//   } else if (swipeDistance < -50) {
+//     swipeDown();
+//   }
+// };
 
-const swipeDown = () => {
-  if (unit > maxUnit) {
-    unit = unit - cardHeight;
-    slide.style.top = unit + "px";
-  } else {
-    unit = 0;
-    slide.style.top = unit + "px";
-  }
-};
+// const swipeDown = () => {
+//   if (unit > maxUnit) {
+//     unit = unit - cardHeight;
+//     slide.style.top = unit + "px";
+//   } else {
+//     unit = 0;
+//     slide.style.top = unit + "px";
+//   }
+// };
 
-const swipeUp = () => {
-  if (unit < 0) {
-    unit = unit + cardHeight;
-    slide.style.top = unit + "px";
-  } else {
-    unit = maxUnit;
-    slide.style.top = unit + "px";
-  }
-};
+// const swipeUp = () => {
+//   if (unit < 0) {
+//     unit = unit + cardHeight;
+//     slide.style.top = unit + "px";
+//   } else {
+//     unit = maxUnit;
+//     slide.style.top = unit + "px";
+//   }
+// };
 
-downArrow.onclick = () => {
-  swipeDown();
-};
+// downArrow.onclick = () => {
+//   swipeDown();
+// };
 
-upArrow.onclick = () => {
-  swipeUp();
-};
+// upArrow.onclick = () => {
+//   swipeUp();
+// };
 
 // CONTACT
 
